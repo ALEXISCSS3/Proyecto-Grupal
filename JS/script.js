667cb31720ab0d85ea00a65cc7e11df5b3921b18
alexis <costillaalexisemanuel14@gmail.com>
2024-03-20 10:22:05 -0300

    Agregué JS

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,69 @@
-const productos = document.querySelector("#productos");
+window.addEventListener("scroll", transicion);
 
-productos.addEventListener("click", function(e) {
-    // Verifica si el clic se realizó en un enlace dentro de #productos
-    if (e.target.tagName === 'A') {
-        e.preventDefault(); // Evita el comportamiento por defecto de los enlaces
-        console.log("bien");
-    }
-});
+function transicion() {
+    let header = document.querySelector("header");
+    header.classList.toggle("transicion", window.scrollY > 0);
+}
+
+let whatsapp = document.querySelector(".whatsapp");
+let numero = document.querySelector(".numero");
+let instagram = document.querySelector(".instagram");
+
+// Agregamos un "event listener" para el evento 'mouseover' en el elemento numero
+numero.addEventListener("mouseover", mostrarNumero);
+
+// Agregamos un "event listener" para el evento 'mouseover' en el elemento whatsapp
+whatsapp.addEventListener("mouseover", mostrarNumero);
+
+// Definimos la función para mostrar el número
+function mostrarNumero() {
+  // Mostramos el número de teléfono
+  numero.style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const imagen1 = document.getElementById("imagen1");
+const imagen2 = document.getElementById("imagen2");
+const imagen3 = document.getElementById("imagen3");
+const imagen4 = document.getElementById("imagen4");
+const imagen5 = document.getElementById("imagen5");
+const imagen6 = document.getElementById("imagen6");
+const imagen7 = document.getElementById("imagen7");
+const imagen8 = document.getElementById("imagen8");
+const imagen9 = document.getElementById("imagen9");
+
+const imagenes = [
+    "recursos/reseña1.jpeg",
+    "recursos/reseña2.jpeg",
+    "recursos/reseña3.jpeg",
+    "recursos/reseña4.jpeg",
+    "recursos/reseña5.jpeg",
+    "recursos/reseña6.jpeg",
+    "recursos/reseña7.jpeg",
+    "recursos/reseña8.jpeg",
+    "recursos/reseña9.jpeg",
+];
+
+let index = 0;
+
+function cambiarImagenes() {
+    const imagen1 = document.getElementById("imagen1");
+    const imagen2 = document.getElementById("imagen2");
+
+    imagen1.src = imagenes[index];
+    imagen2.src = imagenes[(index + 1) % imagenes.length];
+
+    index = (index + 2) % imagenes.length;
+}
+
+setInterval(cambiarImagenes, 5000);
